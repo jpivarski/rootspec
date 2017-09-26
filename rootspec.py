07f9ab7f8195f90b62b11002a3341c1a457f4508
Jim Pivarski <jpivarski@gmail.com>
2017-09-25 23:49:39 -0500

    getting TH1F name by coincidence

--- rootspec.py
+++ rootspec.py
@@ -17,6 +17,7 @@
 import ast
 import re
 import struct
+import string
 import numpy
 import yaml
 from collections import namedtuple
@@ -33,12 +34,39 @@ from lz4.block import decompress as lz4_decompress
 class RootSpecError(Exception): pass
 
 class FixedWidth(struct.Struct):
+    def __init__(self, format, dtype):
+        super(FixedWidth, self).__init__(format)
+        self.dtype = dtype
+
     def __repr__(self):
-        return "FixedWidth({0})".format(repr(self.format))
+        return "FixedWidth({0}, {1})".format(repr(self.format), repr(self.dtype))
 
     def __call__(self, file, index, parent=None):
         return self.unpack(file[index:index + self.size])[0]
-    
+
+    def _sizeof(self, file, index):
+        return self.size
+
+class Postprocess(object):
+    def __init__(self, reader, name, expr):
+        self.reader = reader
+        self.name = name
+        self.expr = expr
+        env = {}
+        for x in dir(numpy):
+            env[x] = getattr(numpy, x)
+        exec(compile(ast.parse("def postprocess({0}):\n  return {1}".format(name, expr)), "<auto>", "exec"), env)
+        self.fcn = env["postprocess"]
+
+    def __repr__(self):
+        return "Postprocess({0}, {1}, {2})".format(self.reader, repr(self.name), repr(self.expr))
+
+    def __call__(self, file, index, parent=None):
+        return self.fcn(self.reader(file, index, parent))
+
+    def _sizeof(self, file, index):
+        return self.reader._sizeof(file, index)
+
 class PascalString(object):
     def __repr__(self):
         return "PascalString()"
@@ -81,17 +109,17 @@ class CString(object):
         return end + 1 - index
 
 readers = {
-    "bool": FixedWidth("?"),
-    "int8": FixedWidth("b"),
-    "uint8": FixedWidth("B"),
-    "int16": FixedWidth(">h"),
-    "uint16": FixedWidth(">H"),
-    "int32": FixedWidth(">i"),
-    "uint32": FixedWidth(">I"),
-    "int64": FixedWidth(">q"),
-    "uint64": FixedWidth(">Q"),
-    "float32": FixedWidth(">f"),
-    "float64": FixedWidth(">d"),
+    "bool": FixedWidth("?", numpy.dtype(numpy.bool_)),
+    "int8": FixedWidth("b", numpy.dtype(numpy.int8)),
+    "uint8": FixedWidth("B", numpy.dtype(numpy.uint8)),
+    "int16": FixedWidth(">h", numpy.dtype(numpy.int16)),
+    "uint16": FixedWidth(">H", numpy.dtype(numpy.uint16)),
+    "int32": FixedWidth(">i", numpy.dtype(numpy.int32)),
+    "uint32": FixedWidth(">I", numpy.dtype(numpy.uint32)),
+    "int64": FixedWidth(">q", numpy.dtype(numpy.int64)),
+    "uint64": FixedWidth(">Q", numpy.dtype(numpy.uint64)),
+    "float32": FixedWidth(">f", numpy.dtype(numpy.float32)),
+    "float64": FixedWidth(">d", numpy.dtype(numpy.float64)),
     "string": PascalString(),
     "cstring": CString()}
 
@@ -107,7 +135,7 @@ def interprettype(format):
     if isinstance(format, dict) and set(format.keys()) == set(["string"]):
         bytes = format["string"]
         assert isinstance(bytes, int)
-        return FixedWidth(repr(bytes) + "s")
+        return FixedWidth(repr(bytes) + "s", None)
 
     elif isinstance(format, dict) and set(format.keys()) == set(["array", "size"]):
         return Array(format["size"], interprettype(format["array"]))
@@ -123,7 +151,7 @@ def expandifs(spec, base, offset, inits):
     fields = OrderedDict()
     for itemindex, item in enumerate(spec):
         if set(item.keys()) == set(["if"]):
-            assert isinstance(item["if"], list)
+            assert isinstance(item["if"], list) and len(item["if"]) > 0
 
             predicates = []
             names = set()
@@ -152,7 +180,10 @@ def expandifs(spec, base, offset, inits):
                 predicates.append(predicate)
                 names.update(thisfields)
                 init.append((predicate, thisafter))
-            
+
+            if init[-1][0] is not None:
+                init.append((None, After("_base{0} + {1}".format(base, offset))))
+
             if itemindex + 1 < len(spec):
                 inits.append(Init(base + 1, init))
                 base += 1
@@ -165,11 +196,16 @@ def expandifs(spec, base, offset, inits):
             assert name not in fields
 
             jumpto = None
+            postprocess = None
             if isinstance(format, dict) and "type" in format:
                 jumpto = format.get("at", None)
+                postprocess = format.get("postprocess", None)
                 format = format["type"]
 
             reader = interprettype(format)
+            if postprocess is not None:
+                reader = Postprocess(reader, name, postprocess)
+
             if jumpto is None:
                 fields[name] = Where(base, offset, reader)
                 initseek = "_base{0} + {1}".format(base, offset)
@@ -202,8 +238,11 @@ def expandifs(spec, base, offset, inits):
 
     return fields, After("_base{0} + {1}".format(base, offset)), lastinit
 
+reservednumpy = ["bool", "int8", "uint8", "int16", "uint16", "int32", "uint32", "int64", "uint64", "float32", "float64"]
 def prependself(expr):
-    if isinstance(expr, ast.Name):
+    if isinstance(expr, ast.Name) and expr.id in reservednumpy:
+        return ast.parse("numpy.{0}".format(expr.id)).body[0].value
+    elif isinstance(expr, ast.Name):
         return ast.parse("self.{0}".format(expr.id)).body[0].value
     elif isinstance(expr, ast.AST):
         for field in expr._fields:
@@ -272,6 +311,7 @@ while i < REPLACEME:
                     assert out is None
                     out = setbase(Init(init.base, consequent))
                 else:
+                    assert out is not None
                     tmp = ast.parse("if REPLACEME:\n  REPLACEME\nelse:  REPLACEME").body
                     tmp[0].test = pythonexpr(predicate)
                     tmp[0].body = setbase(Init(init.base, consequent))
@@ -293,7 +333,7 @@ while i < REPLACEME:
 
     return out, readers
 
-def pythonprop(prop):
+def pythonprop(classname, name, prop):
     readers = {}
     def recurse(prop):
         if isinstance(prop, (Where, Jumpto)):
@@ -334,6 +374,8 @@ return out
                     tmp = ast.parse("if REPLACEME:\n  REPLACEME\nelse:\n  REPLACEME").body
                     tmp[0].test = pythonexpr(predicate)
                     tmp[0].body = recurse(consequent)
+                    if out is None:
+                        out = ast.parse("raise ValueError('property {0}.{1} has no value under this set of conditions')".format(classname, name)).body
                     tmp[0].orelse = out
                     out = tmp
             return out
@@ -350,6 +392,25 @@ class Cursor(object):
 
     def __repr__(self):
         return "<{0} in {1} at {2}>".format(self.__class__.__name__, repr(self._file.filename), self._base0)
+
+    def hexdump(self, at=None, size=160, offset=0):
+        if at is None:
+            at = self._base0
+        elif isinstance(at, Cursor):
+            at = at._base0
+        pos = at + offset
+
+        out = []
+        for linepos in range(pos, pos + size, 16):
+            data = self._file[linepos:min(linepos + 16, pos + size)]
+            line = ["%02x" % x for x in data]
+            text = [chr(x) if chr(x) in string.printable[:-5] else "." for x in data]
+            if len(line) < 16:
+                diff = 16 - len(line)
+                line.extend(["  "] * diff)
+                text.extend([" "] * diff)
+            out.append("{0:08d}  {1}  {2}  |{3}|".format(linepos, " ".join(line[:8]), " ".join(line[8:]), "".join(text)))
+        return "\n".join(out)
 
     @classmethod
     def _sizeof(cls, file, pos):
@@ -396,7 +457,7 @@ def declareclass(classname, spec):
     out = type(classname, (Cursor,), {})
     out._source = OrderedDict([("__init__", pythoninit(inits, spec.get("assert", [])))])
     for name, prop in fields.items():
-        out._source[name] = pythonprop(prop)
+        out._source[name] = pythonprop(classname, name, prop)
     return out
 
 def declare(specification):
@@ -406,7 +467,7 @@ def declare(specification):
 
     for cls in classes.values():
         for name, (source, readers) in cls._source.items():
-            env = {"Cursor": Cursor}
+            env = {"Cursor": Cursor, "numpy": numpy}
             env.update(classes)
             env.update(readers)
             exec(compile(source, "<auto>", "exec"), env)
@@ -480,3 +541,8 @@ for key in keys.keys:
 
 # >>> classes["TKeys"](file, 12069632)
 # 0 12069708
+
+hist = classes["TH1F"](key._file, key.seekkey)
+print repr(hist.named.name), repr(hist.named.title)
+
+print hist.hexdump()
